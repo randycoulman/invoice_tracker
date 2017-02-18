d710173dc34cb9c44917f061c38e74880b235de6
Randy Coulman <rcoulman@gmail.com>
2017-02-18 15:32:25 -0800

    Add credo

--- mix.exs
+++ mix.exs
@@ -28,6 +28,8 @@ defmodule Invoice.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.6", only: [:dev, :test]}
+    ]
   end
 end
