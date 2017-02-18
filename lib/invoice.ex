3b1018a68467352fad89545d102de93c5a6818c9
Randy Coulman <rcoulman@gmail.com>
2017-02-18 15:14:19 -0800

    Generate basic Elixir project

---
+++ lib/invoice.ex
@@ -0,0 +1,18 @@
+defmodule Invoice do
+  @moduledoc """
+  Documentation for Invoice.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Invoice.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/invoice_test.exs
@@ -0,0 +1,8 @@
+defmodule InvoiceTest do
+  use ExUnit.Case
+  doctest Invoice
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
